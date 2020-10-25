5e4b1fc5fbb4e1bc488732fa6dc46c6e848dc7e0
CarlosP18 <Carlosfplaza@hotmail.com>
2020-10-25 21:45:30 +0000

    ejercicio 07.3

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,9 @@
 let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
-// your code here
+let mergeTwoList = (list) => {
+return [...list.filter(a => a % 2 !== 0), ...list.filter(a => a % 2 === 0)]
+}
+
 
 
 console.log(mergeTwoList(list_of_numbers))
